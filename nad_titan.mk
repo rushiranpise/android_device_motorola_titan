9619d96409c5eba66be743a6208d9d69a4523a86
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2021-08-19 13:02:07 +0530

    Update and rename lineage_titan.mk to nad_titan.mk
    nusantara

--- nad_titan.mk
+++ nad_titan.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/motorola/titan/full_titan.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := titan
-PRODUCT_NAME := lineage_titan
-
+PRODUCT_NAME := nad_titan
+TARGET_BOOT_ANIMATION_RES := 720
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
